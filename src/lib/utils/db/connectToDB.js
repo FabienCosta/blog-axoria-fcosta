b37dfacf5563ffca1c795b5eba4f446250d8992c
Fabien Costa <fabiencosta94800@gmail.com>
2025-08-27 14:02:39 +0200

    test vercel 4

--- src/lib/utils/db/connectToDB.js
+++ src/lib/utils/db/connectToDB.js
@@ -1,13 +1,32 @@
+// import mongoose from "mongoose";
+
+// export async function connectToDB() {
+//   if (mongoose.connection.readyState) {
+//     return;
+//   }
+//   try {
+//     await mongoose.connect(process.env.MONGO);
+//     console.log("Connected to database:", mongoose.connection.name);
+//   } catch (err) {
+//     throw new Error("Failed to connect to the Database");
+//   }
+// }
+
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export async function connectToDB() {
-  if (mongoose.connection.readyState) {
-    return;
-  }
+  if (isConnected) return;
+
   try {
-    await mongoose.connect(process.env.MONGO);
-    console.log("Connected to database:", mongoose.connection.name);
+    await mongoose.connect(process.env.MONGO, {
+      dbName: "axoriablogeducation",
+    });
+    isConnected = true;
+    console.log("✅ Connected to database:", mongoose.connection.name);
   } catch (err) {
-    throw new Error("Failed to connect to the Database");
+    console.error("❌ Failed to connect to the Database:", err);
+    throw err; // permet de voir l’erreur exacte dans les logs Vercel
   }
 }
