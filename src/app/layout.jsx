65917f3a409bc34ae43ddfc6c8aa3a84bef76e3b
Fabien Costa <fabiencosta94800@gmail.com>
2025-09-09 14:26:54 +0200

    fix mardown mobile bug

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,25 +1,3 @@
-// import "./globals.css";
-// import Footer from "@/components/Footer";
-// import Navbar from "@/components/Navbar";
-// import { AuthProvider } from "./authContext";
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en" className="h-full" suppressHydrationWarning>
-//       <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-//       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-//       <body className="min-h-full flex flex-col">
-//         <AuthProvider>
-//           <Navbar />
-//           <main className="grow relative">{children}</main>
-//         </AuthProvider>
-//         <Footer />
-//       </body>
-//     </html>
-//   );
-// }
-
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
@@ -37,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr" className="h-full" suppressHydrationWarning>
+    <html lang="en" className="h-full" suppressHydrationWarning>
       <body className="min-h-full flex flex-col">
         <AuthProvider>
           <Navbar />
